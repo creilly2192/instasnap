3f63219dc30696a3499988724090381c8b60abf5
Christina Reilly <creilly2192@gmail.com>
2016-06-15 10:29:43 -0500

    add edit functionality to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,21 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def post_params
