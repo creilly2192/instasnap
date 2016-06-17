7a033d9a157118e9bc24ab3b1d06a06b847c5866
Christina Reilly <creilly2192@gmail.com>
2016-06-16 22:29:05 -0500

    add ajax for smooth comment creation and deletion

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,11 +5,12 @@ class CommentsController < ApplicationController
     @comment = @post.comments.build(comment_params)
     @comment.user_id = current_user.id
     @comment.post_id = @post.id
-    @comment.save
 
     if @comment.save
-      flash[:success] = "You commented the hell out of that post!"
-      redirect_to :back
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
     else
       flash[:alert] = "Check the comment form, something went horribly wrong."
       render root_path
@@ -19,9 +20,13 @@ class CommentsController < ApplicationController
   def destroy
     @comment = @post.comments.find(params[:id])
 
-    @comment.destroy
-      flash[:success] = "Comment deleted :("
-      redirect_to root_path
+    if @comment.user_id == current_user.id
+      @comment.delete
+      respond_to do |format|
+        format.html { redirect_to root_path}
+        format.js
+      end
+    end
   end
 
   private
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   validates :image, attachment_presence: true
   validates :user_id, presence: true
+  validates :caption, length: { maximum: 300 }
 
   has_attached_file :image, styles: { :medium => "640px" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
