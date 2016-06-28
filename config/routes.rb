9f26afbc2131888a1ae4aad7fc384b914394afd5
Christina Reilly <creilly2192@gmail.com>
2016-06-28 16:01:27 -0500

    add follow/unfollow ability for user profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  post ':user_name/follow_user', to: 'relationships#follow_user', as: :follow_user
+  post ':user_name/unfollow_user', to: 'relationships#unfollow_user', as: :unfollow_user
+
   get 'profiles/show'
 
   devise_for :users, :controllers => { registrations: 'registrations' }
