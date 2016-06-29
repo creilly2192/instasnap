22a12b02c71dbc9831361fb278d3ad3971f94421
Christina Reilly <creilly2192@gmail.com>
2016-06-29 14:48:33 -0500

    make ready for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  Paperclip.options[:command_path] = "/usr/local/bin/convert"
+
+  config.action_mailer.default_url_options = { host: 'https://immense-inlet-13412.herokuapp.com'}
 end
