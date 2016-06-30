d81ba6b1e3df6ba6145ceba220fc371c42bdddcd
Christina Reilly <creilly2192@gmail.com>
2016-06-30 16:23:01 -0500

    set up s3

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,6 +10,9 @@ class Post < ActiveRecord::Base
   validates :image, presence: true
   validates :caption, length: { minimum: 3, maximum: 300 }
 
-  has_attached_file :image, styles: { :medium => "640x" }
+  has_attached_file :image,
+    styles: {
+      :medium => "640"
+    }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
